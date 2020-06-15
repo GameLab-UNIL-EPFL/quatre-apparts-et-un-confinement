7920cb69b900d9305d388e2b20f4b43ab3536166
Paul Ronga <paul.ronga@laposte.net>
2020-06-16 00:11:23 +0200

    add spritesheet (wip) and tween animation

--- src/scenes/objects/ProtoSceneObjects/phone.js
+++ src/scenes/objects/ProtoSceneObjects/phone.js
@@ -30,18 +30,20 @@ export class Phone extends CardObject {
         //Call base contructor
         super(
             parent_scene,
-            new Phaser.Math.Vector2(x, y),
             "phone",
-            "/sprites/ProtoScene/WakeUpCard/phone.png",
+            "sprites/ProtoScene/WakeUpCard/phone.png", // URL relatif: on va surement pas heberger à la racine
+            new Phaser.Math.Vector2(x, y),
             false
         );
 
         //All of the "ringing" sprite urls
-        this.ring_urls = [
-            "/sprites/ProtoScene/WakeUpCard/ring1.png",
-            "sprites/ProtoScene/WakeUpCard/ring2.png",
-            "sprites/ProtoScene/WakeUpCard/ring3.png"
-        ];
+        
+        // Plutot une sprite sheet!
+        // this.ring_urls = [
+        //     "sprites/ProtoScene/WakeUpCard/ring1.png",
+        //     "sprites/ProtoScene/WakeUpCard/ring2.png",
+        //     "sprites/ProtoScene/WakeUpCard/ring3.png"
+        // ];
         
         this.cur_state = phoneState.IDLE;
     }
@@ -52,11 +54,13 @@ export class Phone extends CardObject {
     preload() {
         super.preload();
 
+        this.parent_scene.load.spritesheet('ring', 'sprites/ProtoScene/WakeUpCard/ring_spritesheet.png', { frameWidth: 300, frameHeight: 99 });
+        
         //Load the ringing sprites
-        let i = 1;
+        /*let i = 1;
         this.ring_urls.forEach(url => {
             this.parent_scene.load.image("ring" + i++, url);
-        });
+        });*/
     }
 
     /**
@@ -66,13 +70,36 @@ export class Phone extends CardObject {
         super.create();
         this.sprite.setOrigin(0, 0);
         this.sprite.setScale(1);
+        
+        // Create ring sprites
 
-        //Create ring sprites
+        let parent_scene_this_gets_really_verbose = this.parent_scene;
+        this.parent_scene.anims.create({
+          key: 'phone-ring',
+          frameRate: 12,
+          frames: this.parent_scene.anims.generateFrameNames('ring'),
+          repeat: 5
+        });
+        let ringSprite = this.parent_scene.add.sprite(this.x + 150, this.y - 70, 'ring').play('phone-ring');
+        
+        // A améliorer, juste pour montrer le principe
+        this.parent_scene.tweens.add({
+          targets: this.sprite,
+          x: this.sprite.x + PHONE_MOVEMENT,
+          y: this.sprite.y,
+          duration: 30,
+          ease: "Power2", // changer pour plus de “idle”, ou mettre un “onComplete” par exemple
+          yoyo: true,
+          loop: -1,
+        });
+        
+        /*
         let ring1_sprite = this.parent_scene.add.image(this.x + 130, this.y - 70, "ring1");
         let ring2_sprite = this.parent_scene.add.image(this.x + 150, this.y - 70, "ring2");
         let ring3_sprite = this.parent_scene.add.image(this.x + 170, this.y - 10, "ring3");
-
-        this.ring_sprites = [ring1_sprite, ring2_sprite, ring3_sprite];
+        */
+        
+        // this.ring_sprites = [ring1_sprite, ring2_sprite, ring3_sprite];
 
         //Make the phone interactive
         this.sprite.setInteractive();
@@ -85,7 +112,8 @@ export class Phone extends CardObject {
                 if(gameObject === this.sprite) {
                     this.cur_state = phoneState.DONE;
 
-                    this.ring_sprites.forEach(sp => sp.destroy());
+                    // this.ring_sprites.forEach(sp => sp.destroy());
+                    this.sprite.destroy();
 
                     //Trigger the dialogue
                     this.parent_scene.dialogue.display(WAKE_UP_ID);
@@ -101,7 +129,7 @@ export class Phone extends CardObject {
     update() {
         super.update();
         //Check the current state of the phone
-        switch(this.cur_state) {
+        /*switch(this.cur_state) {
             case phoneState.IDLE:
                 //Start the phone's vibration
                 this.cur_state = phoneState.MOVE_LEFT;
@@ -126,6 +154,6 @@ export class Phone extends CardObject {
 
             default:
                 break;
-        }
+        }*/
     }
 }
