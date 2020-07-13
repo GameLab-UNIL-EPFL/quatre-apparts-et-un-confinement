0c29d13937eecec43b94ec4570964bcafec4659f
Paul Ronga <paul.ronga@laposte.net>
2020-07-13 14:42:56 +0200

    move sprites on click

--- src/scenes/storeScene.js
+++ src/scenes/storeScene.js
@@ -6,6 +6,7 @@ import { DialogueController } from "../core/dialogueController.js";
 
 export const StoreCards = {
     FIRST_SHELF: 0,
+    SECOND_SHELF: 1
 };
 
 export class StoreScene extends Phaser.Scene {
@@ -21,17 +22,21 @@ export class StoreScene extends Phaser.Scene {
                 new Background(
                     this,
                     "sprites/StoreScene/part1/rayon01_00_model_light.jpg",
-                    "damienHallwayBGClosed"
+                    "storeBg1"
                 ),
                 new CardObject(
                     this,
                     { name: "pasta1", url: "sprites/StoreScene/part1/rayon01_03_pate01.png" },
                     new Phaser.Math.Vector2(-326, -380),
+                    (scene) => scene.takeObject("pasta1"),
+                    this
                 ),
                 new CardObject(
                     this,
                     { name: "pasta2", url: "sprites/StoreScene/part1/rayon01_03_pate02.png" },
-                    new Phaser.Math.Vector2(-182, -378)
+                    new Phaser.Math.Vector2(-182, -378),
+                    (scene) => scene.takeObject("pasta2"),
+                    this
                 ),
                 new CardObject(
                     this,
@@ -46,8 +51,25 @@ export class StoreScene extends Phaser.Scene {
             ]
         );
 
+        this.secondShelf = new Card(this, [
+                new Background(
+                    this,
+                    "sprites/StoreScene/part2/rayon01_00_model_light.jpg",
+                    "storeBg1"
+                ),
+                new CardObject(
+                    this,
+                    { name: "pasta1", url: "sprites/StoreScene/part1/rayon01_02_pain03.png" },
+                    new Phaser.Math.Vector2(1274, -380),
+                    (scene) => scene.takeObject("pain3"),
+                    this
+                )
+            ]
+        );
+
         this.cards = [
-            this.firstShelf
+            this.firstShelf,
+            this.secondShelf
         ];
 
         //Keep track of wich card is displayed
@@ -62,7 +84,7 @@ export class StoreScene extends Phaser.Scene {
         //Check if any saved data exists
         if(data) {
             console.log("INIT_STORE");
-         }
+        }
     }
 
     preload() {
@@ -71,6 +93,54 @@ export class StoreScene extends Phaser.Scene {
 
         //Preload all of the cards
         this.cards.forEach(card => card.preload());
+
+        this.nextCardArrow = this.load.spritesheet(
+            'next-card-arrow',
+            'sprites/StoreScene/part1/rayon01_06-rayonsuivant-spritesheet_100x100.png',
+            { frameWidth: 100, frameHeight: 100 }
+        );
+    }
+
+    addArrow() {
+        // Create ring sprites
+        this.nextCardButton = this.anims.create({
+            key: 'next-card-anim',
+            frameRate: 15,
+            frames: this.anims.generateFrameNames('next-card-arrow'),
+            repeat: -1
+        });
+
+        //Play the cat animation
+        this.nextCardButton = this.add.sprite(
+            470,
+            120,
+            'next-card-arrow'
+        ).play('next-card-anim');
+
+        let scene = this;
+        this.nextCardButton.setInteractive().on('pointerdown', () => scene.nextCard() );
+    }
+
+    takeObject(object_name) {
+      console.log('take', object_name);
+      let object = this.children.getByName(object_name);
+      object.depth = 5;
+      this.tweens.add({
+          targets: object,
+          x: 290,
+          y: 669,
+          duration: 500,
+          ease: 'Quadratic',
+          yoyo: false,
+          loop: 0
+      });
+
+      /*
+      // si on veut le faire tomber
+      let physicalObject = this.physics.add.sprite(object.x, object.y, object_name);
+      physicalObject.setGravityY(500);
+      */
+
     }
 
     create() {
@@ -79,16 +149,37 @@ export class StoreScene extends Phaser.Scene {
 
         if(this.current_card.isLoaded()) {
             this.current_card.create();
+            this.addArrow()
         }
 
         //Update the saved data
-        // player.cur_scene = Scenes.HALLWAY;
+        // ! Cette scene apparaitra pour deux personnages: independant, etudiant
+        // player.cur_scene = Scenes.STORE;
+    }
+
+    nextCard(){
+      console.log('next card!')
+      if(this.cardIdx < this.cards.length - 1){
+        this.cardIdx++;
+        this.current_card = this.cards[this.cardIdx];
+
+        this.current_card.create();
+
+        this.tweens.add({
+            targets: this.cameras.main,
+            x: '-4200',
+            duration: 500,
+            ease: 'Quadratic',
+            yoyo: false,
+            loop: 0
+        });
+      }
     }
 
     nextScene(cardIdx) {
         // TODO
         /*this.cameras.main.fadeOut(3000, 0, 0, 0,
-            () => this.scene.start(Scenes.DAMIEN_KITCHEN_CLOTHES, { cardIdx: cardIdx }),
+            () => this.scene.start(Scenes.XXXX, { cardIdx: cardIdx }),
             this
         );*/
     }
