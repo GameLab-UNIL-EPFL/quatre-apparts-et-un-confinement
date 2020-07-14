22370ac5161a54cdc12a310c8fe44ddffe52c932
Paul Ronga <paul.ronga@laposte.net>
2020-07-14 08:49:42 +0200

    more transitions

--- src/scenes/storeScene.js
+++ src/scenes/storeScene.js
@@ -38,38 +38,65 @@ export class StoreScene extends Phaser.Scene {
                     (scene) => scene.takeObject("pasta2"),
                     this
                 ),
+                
                 new CardObject(
                     this,
-                    { name: "liste", url: "sprites/StoreScene/part1/rayon01_04_liste.png" },
-                    new Phaser.Math.Vector2(-306, 489)
+                    { name: "pasta3", url: "sprites/StoreScene/part1/rayon01_03_pate03.png" },
+                    new Phaser.Math.Vector2(-441, -132),
+                    (scene) => scene.takeObject("pasta2"),
+                    this
+                ),
+                
+                new CardObject(
+                    this,
+                    { name: "pasta4", url: "sprites/StoreScene/part1/rayon01_03_pate04.png" },
+                    new Phaser.Math.Vector2(-328, -132),
+                    (scene) => scene.takeObject("pasta2"),
+                    this
                 ),
                 new CardObject(
                     this,
-                    { name: "caddie", url: "sprites/StoreScene/part1/rayon01_02_panier.png" },
-                    new Phaser.Math.Vector2(290, 669)
+                    { name: "pasta5", url: "sprites/StoreScene/part1/rayon01_03_pate05.png" },
+                    new Phaser.Math.Vector2(-206, -132),
+                    (scene) => scene.takeObject("pasta2"),
+                    this
                 ),
-            ]
+                
+                
+
+            ], null, null, null, null, true
         );
 
         this.secondShelf = new Card(this, [
                 new Background(
                     this,
                     "sprites/StoreScene/part2/rayon01_00_model_light.jpg",
-                    "storeBg1"
+                    "storeBg2"
                 ),
                 new CardObject(
                     this,
-                    { name: "pasta1", url: "sprites/StoreScene/part1/rayon01_02_pain03.png" },
-                    new Phaser.Math.Vector2(1274, -380),
+                    { name: "pain3", url: "sprites/StoreScene/part2/rayon01_02_pain03.png" },
+                    new Phaser.Math.Vector2(23, 191),
                     (scene) => scene.takeObject("pain3"),
                     this
                 )
             ]
         );
+        
+        this.thirdShelf = new Card(this, [
+                new Background(
+                    this,
+                    "sprites/StoreScene/part3/rayon03_00_model.jpg",
+                    "storeBg3"
+                ),
+          
+            ]
+        );
 
         this.cards = [
             this.firstShelf,
-            this.secondShelf
+            this.secondShelf,
+            this.thirdShelf
         ];
 
         //Keep track of wich card is displayed
@@ -94,6 +121,10 @@ export class StoreScene extends Phaser.Scene {
         //Preload all of the cards
         this.cards.forEach(card => card.preload());
 
+        // Sprites for the whole scene (all 3 cards)
+        this.load.image('liste', "sprites/StoreScene/part1/rayon01_04_liste.png");
+        this.load.image('caddie', "sprites/StoreScene/part1/rayon01_02_panier.png");
+
         this.nextCardArrow = this.load.spritesheet(
             'next-card-arrow',
             'sprites/StoreScene/part1/rayon01_06-rayonsuivant-spritesheet_100x100.png',
@@ -102,7 +133,7 @@ export class StoreScene extends Phaser.Scene {
     }
 
     addArrow() {
-        // Create ring sprites
+        // Create arrow sprites
         this.nextCardButton = this.anims.create({
             key: 'next-card-anim',
             frameRate: 15,
@@ -110,9 +141,9 @@ export class StoreScene extends Phaser.Scene {
             repeat: -1
         });
 
-        //Play the cat animation
+        // Position relative to scene width
         this.nextCardButton = this.add.sprite(
-            470,
+            this.cameras.main.width * 0.39,
             120,
             'next-card-arrow'
         ).play('next-card-anim');
@@ -122,25 +153,34 @@ export class StoreScene extends Phaser.Scene {
     }
 
     takeObject(object_name) {
+      return; // debug
+      
       console.log('take', object_name);
       let object = this.children.getByName(object_name);
       object.depth = 5;
+      
+      // Move to caddie
       this.tweens.add({
           targets: object,
           x: 290,
           y: 669,
-          duration: 500,
+          duration: 300,
           ease: 'Quadratic',
           yoyo: false,
-          loop: 0
+          loop: 0,
+          onComplete: () => {
+              // Animate caddie
+              this.tweens.add({
+                targets: this.caddie,
+                scale: 1.2,
+                duration: 100,
+                ease: 'Quadratic',
+                yoyo: true,
+                loop: 0
+              })
+          },
+          onCompleteScope: this
       });
-
-      /*
-      // si on veut le faire tomber
-      let physicalObject = this.physics.add.sprite(object.x, object.y, object_name);
-      physicalObject.setGravityY(500);
-      */
-
     }
 
     create() {
@@ -151,33 +191,46 @@ export class StoreScene extends Phaser.Scene {
             this.current_card.create();
             this.addArrow()
         }
+        
+        this.checklist = this.add.image(-this.cameras.main.width * 0.255, 489, 'liste');
+        this.checklist.depth = 10;
+        
+        this.caddie = this.add.image(this.cameras.main.width * 0.24166, 669, 'caddie');
 
-        //Update the saved data
+        // Update the saved data
         // ! Cette scene apparaitra pour deux personnages: independant, etudiant
         // player.cur_scene = Scenes.STORE;
     }
 
     nextCard(){
-      console.log('next card!')
-      if(this.cardIdx < this.cards.length - 1){
+        if(this.cardIdx < this.cards.length - 1){
+        
+        // move previous card
+        let container = this.add.container();
+        container.depth = 10;
+        for(let i in this.current_card.children){
+          container.add(this.current_card.children[i].sprite)
+        }
+        this.tweens.add({
+          targets: container,
+          x: -1200,
+          duration: 500,
+          ease: 'Quadratic',
+          yoyo: false,
+          loop: 0
+        })
+        
         this.cardIdx++;
         this.current_card = this.cards[this.cardIdx];
-
         this.current_card.create();
-
-        this.tweens.add({
-            targets: this.cameras.main,
-            x: '-4200',
-            duration: 500,
-            ease: 'Quadratic',
-            yoyo: false,
-            loop: 0
-        });
+        if(this.cardIdx < this.cards.length - 1){
+          this.addArrow();
+        }
       }
     }
-
+    
     nextScene(cardIdx) {
-        // TODO
+        // TODO -- depends on current position in story
         /*this.cameras.main.fadeOut(3000, 0, 0, 0,
             () => this.scene.start(Scenes.XXXX, { cardIdx: cardIdx }),
             this
