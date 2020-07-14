f1898d86390f9c2ca34b89c0b52765d79cf3ddb1
Paul Ronga <paul.ronga@laposte.net>
2020-07-14 13:20:43 +0200

    sort sprites by name

--- _utils/image_names.py
+++ _utils/image_names.py
@@ -1,19 +1,26 @@
 from glob import glob
+import pandas as pd
 
-path = '../sprites/StoreScene/part1/*'
+path = '../sprites/StoreScene/part2/*'
 
 sprites = glob(path)
 
 template = """new CardObject(
-this,
-{ name: "NAME", url: "PATH" },
-new Phaser.Math.Vector2(0, -300),
-(scene) => scene.takeObject("NAME"),
-this
+    this,
+    { name: "NAME", url: "PATH" },
+    new Phaser.Math.Vector2(0, -300),
+    (scene) => scene.takeObject("NAME"),
+    this
 ),
 """
+
+rows = []
 
 for sprite_path in sprites:
     path = sprite_path.replace('../', '')
     name = sprite_path.split('_')[-1].split('.')[0]
-    print(template.replace("NAME", name).replace("PATH", path))
+    code = template.replace("NAME", name).replace("PATH", path)
+    rows.append([name, code])
+df = pd.DataFrame(rows, columns = ['name', 'text'])
+
+df.sort_values('name')['text'].apply(lambda t: print(t))
