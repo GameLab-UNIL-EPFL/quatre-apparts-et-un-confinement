d6f60e46eb44cf6d266b0b90abd79498e5b68e39
Saara <astonedf@users.noreply.github.com>
2020-07-17 14:18:29 +0200

    page sound fix

--- src/scenes/cards/GrandmaScene/livingRoomCard.js
+++ src/scenes/cards/GrandmaScene/livingRoomCard.js
@@ -146,6 +146,9 @@ export class LivingRoomCard extends Card {
         this.children[this.children.length - 2].sprite.setDepth(3);
         this.children[this.children.length - 2].highlight_sprite.setDepth(3);
 
+        //Add sound to the scene
+        this.page = this.parent_scene.sound.add("pageTurn");
+
         //=========HANDLE_ANIMATIONS=========
 
         // Create ring sprites
@@ -305,7 +308,7 @@ export class LivingRoomCard extends Card {
                     "grandma_book1"
                 );
 
-                this.page = this.parent_scene.sound.add("pageTurn");
+                
                 this.page.play();
 
                 //Trigger the book's dialogue
