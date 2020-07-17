7f170da85aef44da218cce2393f0c049edb60887
Paul Ronga <paul.ronga@laposte.net>
2020-07-17 14:08:29 +0200

    typo

--- webpack/base.js
+++ webpack/base.js
@@ -6,64 +6,64 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml|ttf)$/i,
-        use: "file-loader"
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
-    }),
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
-    }),
-    new CopyWebpackPlugin([
-        {
-            from: 'sound',
-            to: 'sound'
-        },
-        {
-            from: 'sprites',
-            to: 'sprites'
-        },
-        {
-          from: 'fonts',
-          to: 'fonts'
-        }
-    ])
-  ],
-  stats: {
-    logging: 'verbose',
-    maxModules: 10000
-  }
+    mode: "development",
+    devtool: "eval-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"]
+            },
+            {
+                test: [/\.vert$/, /\.frag$/],
+                use: "raw-loader"
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|xml|ttf)$/i,
+                use: "file-loader"
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            root: path.resolve(__dirname, "../")
+        }),
+        new webpack.DefinePlugin({
+            CANVAS_RENDERER: JSON.stringify(true),
+            WEBGL_RENDERER: JSON.stringify(true)
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'sounds',
+                to: 'sounds'
+            },
+            {
+                from: 'sprites',
+                to: 'sprites'
+            },
+            {
+                from: 'fonts',
+                to: 'fonts'
+            }
+        ])
+    ],
+    stats: {
+        logging: 'verbose',
+        maxModules: 10000
+    }
 };
