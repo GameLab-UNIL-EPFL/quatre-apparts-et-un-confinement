ded80fac9e98326be118435c968f0b735d8d71ed
Andrew Dobis <andrew.dobis@epfl.ch>
2020-08-15 12:01:30 +0200

    changed centering mode to center both

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ const config = {
     resolution: 1, // we could use 2 for Retina
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         width: scale.width,
         height: scale.height
     },
